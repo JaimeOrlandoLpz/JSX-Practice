fe20d7965e7a77f153f55233b9fcbf84be866cfe
Jaime Orlando Lopez Ramos <30807749+JaimeOrlandoLpz@users.noreply.github.com>
2021-08-12 13:23:06 -0500

    Exercise completed

--- src/index.js
+++ src/index.js
@@ -1,4 +1,17 @@
 //Create a react app from scratch.
+import React from "react";
+import ReactDOM from "react-dom";
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+ReactDOM.render(
+  <div>
+    <h1> My List </h1>
+    <ul>
+      <li>Watch the Video</li>
+      <li>Do the Task</li>
+      <li>Commit to github</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
